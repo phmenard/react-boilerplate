77078dd142510fc901f044cf244413c95763de52
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-10-08 03:07:56 -0400

    added footer msg

---
+++ app/components/footer/messages.js
@@ -0,0 +1,15 @@
+/*
+ * Header Messages
+ *
+ * This contains all the text for the Header container.
+ */
+import { defineMessages } from 'react-intl';
+
+export const scope = 'app.containers.HomePage';
+
+export default defineMessages({
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'Hello from footer!',
+  },
+});
